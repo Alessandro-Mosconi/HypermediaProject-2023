40efc9d624a54fe79cef5131d0746c4fb36c676a
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-14 18:32:48 +0200

    Update [id].vue

--- pages/persone/[id].vue
+++ pages/persone/[id].vue
@@ -5,7 +5,7 @@
 <template>
     <main>
         <div class = "info-group">
-            <img id = "main-img" src = "~/assets/img/home-planet_tmp.png" />
+            <img src = "@/assets/img/home_planet_tmp.png"/>
             <div id = "data-container">
                 <p class = "data">Name: <span>{{ location.name }}</span></p>
                 <p class = "data">City: <span>{{ location.city }}</span></p>
