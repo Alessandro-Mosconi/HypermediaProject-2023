4e7f5ccbdd6c2a35f76bca1c3522cfbf83ac58cb
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-14 17:41:08 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,14 @@
 -->
 <template>
     <main>
-        <div class="container">
-        <img id = "main-img" src = "~/assets/img/home_planet_tmp.png" />
-        <div class="SuperTitle">about us</div>
-        </div> 
-    </main>
+    <CenterTitlewithImage />
     <Manifest />
+</main>
 </template>
 
 <style>
     main {
         width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        color: whitesmoke;
-    }
-
-    SuperTitle{
-        font-size: xx-large;
-    }
-
-    .container {
-    position: relative;
-    text-align: center;
-    color: white;
+        position: relative;
     }
 </style>
