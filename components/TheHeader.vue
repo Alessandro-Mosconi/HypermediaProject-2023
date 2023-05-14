79779c948ac24148df49b27d2338ad4f28743669
Alessandro-Mosconi <alessandro2.mosconi@mail.polimi.it>
2023-05-14 16:01:59 +0200

    menu
    modifica menu

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,6 +3,10 @@
 -->
 <template>
     <header>
+        
+        <NuxtLink to = "/">
+            <img class="logo" src="@/assets/img/rocket.png" alt="pace Odyssey Capital">
+        </NuxtLink>
         <h1>Space Odyssey Capital</h1> 
         <nav>
             <NuxtLink to = "/">HOME</NuxtLink>
@@ -18,6 +22,9 @@
 <style>
     header
     {
+        margin-top: 2%;
+        margin-bottom: 2%;
+        font-family: ABCWhyteLight;
         background-color: rgba(255, 166, 0, 0);
         color: whitesmoke;
         padding: 0px 4% 0px 4%;
@@ -25,6 +32,44 @@
         align-items: center;
         width:92%;
         justify-content: space-between;
+    }
+
+    .logo {
+        height: 45px;
+        width: 45px;
+        margin-right: 15px;
+    }
+
+    nav {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    a {
+        font-size: 0.6em;
+        margin: 0 15px;
+    }
+
+    a:first-of-type {
+        margin-left: 0;
+    }
+
+    a:last-of-type {
+        margin-right: 0;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    @font-face {
+        font-family: 'ABCWhyteLight';
+        src: url('@/assets/fonts/ABCWhyte/ABCWhyte-Extralight-Trial.otf') format('truetype');
     }
 
     nav {
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,10 @@
     Default layout used by all the page
 -->
 <template>
+    <head>
+        <title>Space Odyssey Capital</title>
+        <meta charset="utf-8">
+    </head>
     <div class = 'page'>
         <TheHeader />
         <slot />
@@ -11,10 +15,17 @@
 
 <style>
     .page {
+        font-family: ABCWhyte;
         min-height: 100vh;
         display: flex;
         flex-direction: column;
         background-color: rgb(0, 0, 0);
         margin: 0;
     }
+
+    @font-face {
+        font-family: 'ABCWhyte';
+        src: url('@/assets/fonts/ABCWhyte/ABCWhytePlusVariable-Trial.ttf') format('truetype');
+    }
+
 </style>
