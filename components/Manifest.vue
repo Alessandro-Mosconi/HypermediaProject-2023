5e7afe4e5f8a56c064d15fe77b9f6f2c10567922
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-14 17:44:42 +0200

    Update Manifest.vue

--- components/Manifest.vue
+++ components/Manifest.vue
@@ -2,38 +2,21 @@
     Header of the page
 -->
 <template>
-    <header>
-        <t>NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS
-        </t>
-    </header>
+        <text>NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS NUKE MARS 
+        </text>
 </template>
 
 <style>
-    header
-    {
+    text{
         margin-top: 2%;
         margin-bottom: 2%;
         background-color: rgba(255, 166, 0, 0);
-        font: Heavy;
         color: whitesmoke;
         padding: 0px 4% 0px 4%;
-        display: flex;
-        align-items: center;
         text-align: justify;
         width:92%;
-        justify-content: space-between;
-    }
-
-    t{
         font-family: Bold;
         font-size: xx-large;
         text-transform: uppercase;
-    }
-
-    nav {
-        display: flex;
-        gap: 30px;
-        font-size: 17pt;
-        font-weight: bold;
     }
 </style>
