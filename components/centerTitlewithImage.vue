276f4d38e893f91248fbe64c377a24c006cc91fe
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-14 17:41:03 +0200

    centerTitlewithImage created

---
+++ components/centerTitlewithImage.vue
@@ -0,0 +1,26 @@
+<!--
+    Centered title with image background
+-->
+<template>
+    <main>
+        <img src = "~/assets/img/home_planet_tmp.png"/>
+        <div class="centeredTitle">ABOUT US</div>
+    </main>
+</template>
+
+<style>
+    main {
+        width: 100%;
+        position: relative;
+    }
+
+    .centeredTitle{
+        position: absolute;
+        color: white;
+        font-size: 1000%;
+        font-family: bold;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
