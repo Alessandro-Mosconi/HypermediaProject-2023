b54a690e58ba9325555e70113b3e5adee98e0302
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-05-15 17:53:55 +0200

    fixed home page and added area components

--- pages/areas.vue
+++ pages/areas.vue
@@ -3,16 +3,16 @@
 -->
 <template>
     <main>
-    <CenterTitlewithImage message="INVESTMENT AREAS"/>
-    <CenterTitlewithImage message="AREA 1"/>
-    <CenterTitlewithImage message="AREA 2"/>
-    <CenterTitlewithImage message="AREA 3"/>
-</main>
+        <CenterTitlewithImage title="INVESTMENT AREAS" description="" />
+        <AreaComponent title="AREA 1" image="https://hips.hearstapps.com/hmg-prod/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg" />
+        <AreaComponent title="AREA 2" image="@/assets/img/space_image_tmp.jpg" />
+        <AreaComponent title="AREA 3" image="@/assets/img/space_image_tmp.jpg" />
+    </main>
 </template>
 
 <style>
-    main {
-        width: 100%;
-        position: relative;
-    }
+main {
+    width: 100%;
+    position: relative;
+}
 </style>
