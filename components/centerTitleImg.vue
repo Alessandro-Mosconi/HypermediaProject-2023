eb42a457aed0fd5c50c1613349f3d56c5f5f0578
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-05-19 15:12:24 +0200

    finished areas page

--- components/centerTitleImg.vue
+++ components/centerTitleImg.vue
@@ -17,6 +17,7 @@ export default {
     props: {
         title: String,
         description: String,
+        img_url: String,
     },
 }
 </script>
--- pages/areas.vue
+++ pages/areas.vue
@@ -4,9 +4,9 @@
 <template>
     <main>
         <centerTitleImg title="INVESTMENT AREAS" description="" />
-        <AreaComponent title="AREA 1" description="DISCOVER MORE" />
-        <AreaComponent title="AREA 2" description="DISCOVER MORE" />
-        <AreaComponent title="AREA 3" description="DISCOVER MORE" />
+        <AreaComponent id="area1" title="AREA 1" description="DISCOVER MORE" />
+        <AreaComponent id="area2" title="AREA 2" description="DISCOVER MORE" />
+        <AreaComponent id="area3" title="AREA 3" description="DISCOVER MORE" />
     </main>
 </template>
 
@@ -15,4 +15,15 @@ main {
     width: 100%;
     position: relative;
 }
+
+#area2 {
+    position: relative;
+    bottom: 4em;
+}
+
+#area3 {    
+    position: relative;
+    bottom: 8em;
+}
+
 </style>
