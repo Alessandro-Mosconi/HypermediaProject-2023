9d89bd817dd4a089b9842b74dab5ea8da9a0ee51
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-22 23:18:14 +0200

    added uncompleted component

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,13 @@
             title="SHAPING THE FUTURE OF SPACE" 
             img="https://kcrxtzylutpqgnipxzbq.supabase.co/storage/v1/object/public/wallpaper/earth_homepagee.png"/>
     </main>
+    <SingleParagraph
+        paragraph="We are Visionaries aiming for a boundless universe."   
+        ></SingleParagraph>
     <SingleItemImgBg
         img="https://kcrxtzylutpqgnipxzbq.supabase.co/storage/v1/object/public/wallpaper/satellite_homepage.jpg">
     </SingleItemImgBg>
+    
 </template>
 
 <style>
