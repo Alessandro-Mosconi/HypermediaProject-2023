d3541f750d2565b71879c335d1e0f866dad26b12
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-24 00:18:39 +0200

    component create for Homepage

---
+++ components/leftTitle.vue
@@ -0,0 +1,38 @@
+<!--
+    Big text with paragraph on the right
+-->
+<template>
+    <div>
+      <p>{{ paragraph }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      paragraph: String,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  p {
+     width:45%;
+     text-align: left;
+     margin-left:5%;
+     font-size: 3vw;
+     font-weight: 500;
+     justify-self:flex-start;
+  }
+  
+  @media only screen and (max-width :1200px) {  
+   .component{
+       flex-direction : column
+   }
+   p{
+       text-align:left;
+       margin-left:10%;
+   } 
+  }
+  </style>
