e29a380f858f00ac0afdf409fb2f7df01f8e3e9d
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-05-24 00:18:52 +0200

    Create rightParagraph.vue
    for the homepage

---
+++ components/rightParagraph.vue
@@ -0,0 +1,53 @@
+<!--
+    Big text with paragraph on the right
+-->
+
+<template>
+  <container class="component">
+  <container class="container">
+    <p>{{ paragraph }}</p>
+    <div class="capsule">FIND OUT MORE ↗</div>
+  </container>
+</container>
+</template>
+
+<script>
+export default {
+  props: {
+    paragraph: String,
+  },
+};
+</script>
+
+<style scoped>
+
+.component{
+  width:100%;
+  display:flex;
+  justify-content: flex-end;
+}
+.container{
+   width:45%;
+   margin-right: 5%;
+   flex-direction:row-reverse;
+}
+
+p {
+ width:100%;
+ font-size: 2vw;
+ font-weight:300; 
+ text-align:left;
+}
+
+.capsule{
+   width: 50%;
+   justify-content:center; 
+   text-align:center; 
+   border:solid white 3px; 
+   border-radius:50px; 
+   text-transform:uppercase; 
+   font-weight: 300;
+   font-size: 2vw;
+   display: inline-block;
+}
+</style>
