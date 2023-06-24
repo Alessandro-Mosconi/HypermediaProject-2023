8e3aa175676adf9f17d66a630d0e16bc6ba7d1c3
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-06-24 18:56:59 +0200

    created single area component

--- pages/areas/index.vue
+++ pages/areas/index.vue
@@ -1,22 +1,25 @@
 <!--
-    Investements area 
+    Investments area
 -->
 <template>
     <main>
-        <centerTitleImg title="WHERE WE INVEST" 
-        img="https://kcrxtzylutpqgnipxzbq.supabase.co/storage/v1/object/public/wallpaper/earth_homepagee.png"
-        description="" />
-        <AreaList :areas="areas"/>
+        <centerTitleImg title="WHERE WE INVEST"
+                        img="https://kcrxtzylutpqgnipxzbq.supabase.co/storage/v1/object/public/wallpaper/earth_homepagee.png"
+                        description=""/>
+        <div class="p-[5%] mt-20">
+            <AreaList :areas="areas"/>
+        </div>
+
     </main>
 </template>
 
 <script>
-    export default defineNuxtComponent({
-        async asyncData() {
-            const areas = await $fetch('/api/areas')
-            return {
-                areas
-            }
+export default defineNuxtComponent({
+    async asyncData() {
+        const areas = await $fetch('/api/areas')
+        return {
+            areas
         }
-    })
+    }
+})
 </script>
