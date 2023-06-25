959b9fc7d11c48c8b5c1781beb0fb7583aef9f2c
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-06-25 08:58:18 +0200

    convert single person page to tailwind

--- components/AreaBanner.vue
+++ components/AreaBanner.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative w-full">
         <img class="w-full h-[70vh] object-cover" :src="imageUrl" alt="Banner Image"/>
-        <div class="absolute text-[7vh] font-medium leading-[9vh] left-[5vh] bottom-[3vh]">
+        <div class="absolute text-[7vh] font-semibold leading-[8vh] left-[5vh] bottom-[3vh]">
             WE INVEST IN <br /><span class="text-[#F65933]">{{ area.toUpperCase() }}</span>
         </div>
     </div>
