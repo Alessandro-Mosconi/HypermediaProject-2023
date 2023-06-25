c17f094802a9cf7f32f2d998c58aa03c184250de
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-06-25 15:44:32 +0200

    project page created

---
+++ components/ProjectList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div v-for="p in projects" class="justify-center ml-auto mr-auto" >
+        <div>
+                <NuxtLink id="about" class="pg" :to='"/projects/"+p.id'>
+                    <cardProject 
+                    :projectName="p.name"
+                    img="https://media.npr.org/assets/img/2016/04/05/ackbar_wide-f0a673c875361830e84eaa3b98f7f886a750b0e5.jpg"
+                    :area="p.area"
+                    colorArea="green"/>
+                </NuxtLink>
+            </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'projects'
+    ]
+}
+</script>
+
--- components/cardProject.vue
+++ components/cardProject.vue
@@ -1,117 +1,20 @@
-<!--
-    Card to display information in the list page.
-    This component could be achieved with the SmallCard component by using a <slot> to insert the div with the image. The solution used here allows for better control of the layout.
-    Since the information shown can be categorized into title and subtitle, it was preferred to use only one component for both dogs and location.
-
-    PROPS:
-    - title: main information to display
-    - subtitle: second information to display
-    - link: link to the page description
--->
 <template>
-    <div class="card">
-        <div class="image-div" :style="bgImage">
-            <h1>→</h1>
-        </div>
-        <NuxtLink :to = "link" ><h3>{{ projectName }}</h3></NuxtLink>
-        <NuxtLink :to = "link" ><h4 :style="colorArea" >{{ area }}</h4></NuxtLink>
+    <div class="card flex flex-col p-5">
+      <button class="h-full w-full bg-cover bg-center" :style="{ 'background-image': `url('${img}')` }">
+        <h1 class="mt-auto mr-2">→</h1>
+      </button>
+      <NuxtLink :to="link"><h1 class="text-3xl uppercase" >{{ projectName  }}</h1></NuxtLink>
+      <NuxtLink :to="link"><h2 class="uppercase" :class="{ 'text-red-500': colorArea === 'red', 'text-green-500': colorArea === 'green' }">{{ area }}</h2></NuxtLink>
     </div>
 </template>
-
-<script>
-export default {
+  
+  <script>
+  export default {
     props: {
-        projectName: String,
-        area: String,
-        img: String,
-        colorArea: String,
-    },
-    computed: {
-        bgImage () {
-        return `background-image: url("${this.img}");`;
-        },
-        colorArea(){
-            return `color: ${this.colorArea};`;  
-        }
-}
-}
-</script>
-
-<!-- 
-style not scoped so that you change also smallerCardProject.vue
--->
-
-<style>
-    .image-div
-    {
-        display: flex;
-        justify-content:right;
-        width: 400px;
-        height: 250px;
-        background-repeat: no-repeat;
-        background-size: cover;
+      projectName: String,
+      area: String,
+      img: String,
+      colorArea: String
     }
-
-    .card
-    {
-        font-size: 6vw;
-        display: flex;
-        flex-flow: column;
-        padding: 20px;
-        width: fit-content;
-        align-items: flex-start;
-    }
-
-    .card h1{
-        font-family:monospace;
-        margin-top: 0;
-        margin-right: 10px;
-    }
-
-
-    .card h3
-    {
-        font-size: 2.5vw;
-        font-weight: bold;
-        text-transform: uppercase;
-        font-weight: 500;
-        display:contents;  
-    }
-    .card h4
-    {
-        font-size: 1.6vw;
-        font-weight: lighter;
-        text-transform: uppercase;
-        font-weight: 300;
-        display:contents;  
-    }
-
-    .card h3::before {
-    content: '';
-    position: absolute;
-    width: 0;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #ffffff;
-    transition: width 0.3s ease-in-out;
-}
-
-    .card h3:hover::before {
-        width: 100%;
-    }
-
-    @media (min-width: 1920px) {
-    .card h1 {
-    font-size: 3vw;
-    }
-    .card h3 {
-    font-size: 2vw;
-    }
-
-    .card h4 {
-    font-size: 1vw;
-    }
-}
-
-</style>
+  }
+  </script>
