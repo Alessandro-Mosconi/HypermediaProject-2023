e3c74523b44a1daad42f96973d82f6ac1bc8dc22
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-06-25 16:17:24 +0200

    Created best projects page

--- pages/projects/best-projects.vue
+++ pages/projects/best-projects.vue
@@ -1,17 +1,21 @@
 <!--
-    Page with the list of all the projects.
+    Page with the list of BEST projects.
 -->
 <template>
     <centerTitleImg 
-            title="OUR BEST PROJECTS" 
+    title="OUR BEST PROJECTS" 
+            img="https://kcrxtzylutpqgnipxzbq.supabase.co/storage/v1/object/public/wallpaper/earth_homepagee.png"
             />
-    
+    <ProjectList :projects= "projects"/>
 </template>
 
 <script>
-    export default {
-    }
+    export default defineNuxtComponent({
+        async asyncData() {
+            const projects = await $fetch('/api/projects/best-projects')
+            return {
+                projects
+            }
+        }
+    })
 </script>
-
-<style>
-</style>
---
+++ server/api/projects/best-projects.get.js
@@ -0,0 +1,16 @@
+import { serverSupabaseClient } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+    const client = serverSupabaseClient(event)
+
+    const { data, error } = await client
+        .from('project')
+        .select('*')
+        .not('top', 'is', null)
+        .order('id', { ascending: true })
+    if (error) {
+        throw createError({ statusCode: 400, statusMessage: error.message })
+    }
+
+    return data
+})
