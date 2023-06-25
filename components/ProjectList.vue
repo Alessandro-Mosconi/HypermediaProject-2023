1562146355b2d1803cbb5dcd1bb00fa6d76bf2fd
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-06-25 16:18:43 +0200

    changed cardProject

--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,13 +1,13 @@
 <template>
-    <div  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 grid-cols-2 max-w-screen-lg mx-auto">
         <div v-for="p in projects" class="justify-center ml-auto mr-auto" >
         <div>
                 <NuxtLink id="about" class="pg" :to='"/projects/"+p.id'>
                     <cardProject 
                     :projectName="p.name"
-                    img="https://media.npr.org/assets/img/2016/04/05/ackbar_wide-f0a673c875361830e84eaa3b98f7f886a750b0e5.jpg"
+                    :img="p.img_url"
                     :area="p.area"
-                    colorArea="green"/>
+                    :colorArea="green"/>
                 </NuxtLink>
             </div>
     </div>
--- components/cardProject.vue
+++ components/cardProject.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="card flex flex-col p-5">
-      <button class="h-full w-full bg-cover bg-center" :style="{ 'background-image': `url('${img}')` }">
+  <div class="lg:h-72 sm:h-44 h-32" style="height: 250px; width: 300px;">
+    <div class="relative">
+      <div class="absolute h-full w-full z-10">
+        <img :src="img" alt="" class="h-full w-full object-cover"/>
+      </div>
+      <button class="h-full w-full bg-transparent hover:bg-gray-100 transition duration-300 ease-in-out focus:outline-none focus:ring focus:border-blue-300">
         <h1 class="mt-auto mr-2">→</h1>
       </button>
-      <NuxtLink :to="link"><h1 class="text-3xl uppercase" >{{ projectName  }}</h1></NuxtLink>
-      <NuxtLink :to="link"><h2 class="uppercase" :class="{ 'text-red-500': colorArea === 'red', 'text-green-500': colorArea === 'green' }">{{ area }}</h2></NuxtLink>
     </div>
+    <NuxtLink :to="/projects/"><h1 class="text-3xl uppercase" >{{ projectName  }}</h1></NuxtLink>
+      <NuxtLink :to="/projects/"><h2>{{ area }}</h2></NuxtLink>
+  </div>
 </template>
-  
+
   <script>
   export default {
     props: {
       projectName: String,
       area: String,
       img: String,
-      colorArea: String
     }
   }
   </script>
