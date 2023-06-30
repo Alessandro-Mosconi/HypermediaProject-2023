78cca733a9fa6c944453f6c90e0f3549334eeea4
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-06-30 14:41:35 +0200

    added image carousel

--- components/BigCarousel.vue
+++ components/BigCarousel.vue
@@ -1,22 +1,74 @@
 <template>
-    <v-app>
-        <v-carousel>
-            <v-carousel-item v-for="color in colors" :key="color">
-                <v-sheet :color="color" height="100%" tile>
-                    <v-row class="fill-height" align="center" justify="center">
-                        <div class="text-h2">
-                            {{ color }}
-                        </div>
-                    </v-row>
-                </v-sheet>
-            </v-carousel-item>
-        </v-carousel>
-    </v-app>
+    <div id="default-carousel" class="relative w-full" data-carousel="slide">
+        <!-- Carousel wrapper -->
+        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <!-- Item 1 -->
+            <div class="duration-700 ease-in-out" data-carousel-item>
+                <img src="https://shorturl.at/owKU6" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 2 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="https://shorturl.at/fQV38" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+        </div>
+        <!-- Slider indicators -->
+        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+        </div>
+        <!-- Slider controls -->
+        <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev @click="prevImage">
+            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                <span class="sr-only">Previous</span>
+            </span>
+        </button>
+        <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next @click="nextImage">
+            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                <span class="sr-only">Next</span>
+            </span>
+        </button>
+    </div>
 </template>
+
 <script>
+
 export default {
-    data: () => ({
-        colors: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', '#7f00ff'],
-    }),
+    props: {
+        'projects': Array,
+    },
+    methods: {
+        nextImage() {
+            const carousel = document.querySelector('[data-carousel="slide"]');
+            carousel.dispatchEvent(new CustomEvent('next.carousel', { bubbles: true }));
+        },
+        prevImage() {
+            const carousel = document.querySelector('[data-carousel="slide"]');
+            carousel.dispatchEvent(new CustomEvent('prev.carousel', { bubbles: true }));
+        }
+    },
+    mounted() {
+        const carousel = document.querySelector('[data-carousel="slide"]');
+
+        carousel.addEventListener('next.carousel', () => {
+            const currentImage = carousel.querySelector('[data-carousel-item]:not(.hidden)');
+            const nextImage = currentImage.nextElementSibling || carousel.querySelector('[data-carousel-item]:first-child');
+
+            currentImage.classList.add('hidden');
+            nextImage.classList.remove('hidden');
+        });
+
+        carousel.addEventListener('prev.carousel', () => {
+            const currentImage = carousel.querySelector('[data-carousel-item]:not(.hidden)');
+            const prevImage = currentImage.previousElementSibling || carousel.querySelector('[data-carousel-item]:last-child');
+
+            currentImage.classList.add('hidden');
+            prevImage.classList.remove('hidden');
+        });
+    }
 };
 </script>
