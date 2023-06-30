856bfaa6a04b1f98671382e97045d469c28b19a9
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2023-06-30 15:48:10 +0200

    fixed conflicts

--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,46 +1,40 @@
 <template>
-    
-  <div>
-    <div class="flex flex-col sm:flex-row justify-center">
-        <div class="flex flex-col"> 
-      <div v-for="(p, index) in projects" :key="index" class="p-4 mb-[-30px]">
+    <div>
+        <div class="flex flex-col sm:flex-row justify-center">
+            <div class="flex flex-col">
+                <div v-for="(p, index) in projects" :key="index" class="p-4 mb-[-30px]">
+                    <div v-if="index % 2 ===0">
+                        <CardProject
+                            :projectName="p.name"
+                            :projectId="p.id"
+                            :img="p.img_url"
+                            :area="p.area"/>
+                    </div>
+                </div>
+            </div>
+            <!-- second column -->
+            <div class="flex flex-col justify-center">
+                <div v-for="(p, index) in projects" :key="index" class="p-4">
+                    <div v-if="index % 2 ===1">
+                        <!-- empty space -->
+                        <div class="sm:flex hidden" style="
+                              width: 300px;
+                              height: 160px;
+                              background-color: rgba(255, 0, 0, 255);
+                        "></div>
 
-      <div v-if="index % 2 ===0">
-        <NuxtLink id="about" :to="'/projects/' + p.id">
-          <cardProject 
-            :projectName="p.name"
-            :img="p.img_url"
-            :area= "p.area"/>
-         </NuxtLink>
-         </div>
-
+                        <div>
+                            <CardProject
+                                :projectName="p.name"
+                                :projectId="p.id"
+                                :img="p.img_url"
+                                :area="p.area"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-    <!-- second column --> 
-    <div class="flex flex-col justify-center"> 
-      <div v-for="(p, index) in projects" :key="index" class="p-4">
-        <div v-if="index % 2 ===1">
-        <!-- empty space -->
-        <div class="sm:flex hidden" style="
-          width: 300px;
-          height: 160px;
-          background-color: rgba(255, 0, 0, 255);
-        "></div>
-
-      <div>
-        <NuxtLink id="about" :to="'/projects/' + p.id">
-          <cardProject 
-            :projectName="p.name"
-            :img="p.img_url"
-            :area= "p.area"/>
-         </NuxtLink>
-         </div>
-    </div>
-    </div>
-    </div>
-
-     </div>
-   </div>
 </template>
 
 <script>
