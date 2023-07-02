4347a514be50f224466e77d5aa70852a745d2398
Alessandro-Mosconi <alessandro2.mosconi@mail.polimi.it>
2023-07-02 15:49:57 +0200

    aggiunti team e supervisor a project

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -5,8 +5,11 @@
         <ProjectBanner :name="pro.name" :areaCode="pro.area" :img="pro.img_url" ></ProjectBanner>
         <div class="md:ml-10 ml-5 mr-5 mt-[2em]">
             <div class="mt-10 md:mt-20">
-                <div class="mb-10 md:mb-20">
-                    <SmallTextParagraph :smallText="'Supervised by'"></SmallTextParagraph>
+                <div class="mb-10 ">
+                    <SmallTextList :smallText="'Supervised by'" :list="supervisor" />
+                </div>
+                <div class="mb-10 ">
+                    <SmallTextList :smallText="'Team'" :list="team" />
                 </div>
                 <div class="mb-10 md:mb-20">
                     <SmallTextParagraph :smallText="'Project Description'"
@@ -22,9 +25,27 @@ export default defineNuxtComponent({
     async asyncData() {
         const route = useRoute()
         const pro = await $fetch('/api/projects/' + route.params.id)
+        const supervisor = await $fetch('/api/people/' + pro.id_supervisor)
+        const team = await $fetch('/api/people/team/' + pro.id)
+        
+        let listWorkersProject = [];
+        for (let p of team) {
+            listWorkersProject.push({
+                text: p.name + ' ' + p.surname,
+                link: '/team/' + p.id,
+            })
+        }
+
+        let listSupervisor = [];
+        listSupervisor.push({
+            text: supervisor.name + ' ' + supervisor.surname,
+            link: '/team/' + supervisor.id,
+        })
 
         return {
-            pro
+            pro,
+            supervisor: listSupervisor,
+            team: listWorkersProject,
         }
     }
 })
